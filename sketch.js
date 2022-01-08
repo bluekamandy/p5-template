12f5bfb8e305c51228ffad0aa2a62fa9860a638d
Masood Kamandy <masood@masoodkamandy.com>
2022-01-08 15:44:59 -0800

    Adding template files including header.

---
+++ sketch.js
@@ -0,0 +1,23 @@
+/*
+ * Course: DMA 62 - Creative Coding for the Internet
+ * Assignment: Assignment Name
+ *
+ * by First Last
+ * 
+ * Initiated: 1/1/1
+ * Last updated: 1/1/1
+ * 
+ * CITATIONS: List all sources you used for this assignment.
+ *
+ * NOTE: This is where you write any notes you would like
+ * to leave yourself or others using your code.
+ * 
+ * */
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
